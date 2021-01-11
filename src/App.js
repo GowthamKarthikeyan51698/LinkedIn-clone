26dbe0a99d614b11c6710dda4046e05b4650edec
GowthamKarthikeyan51698 <75258383+GowthamKarthikeyan51698@users.noreply.github.com>
2021-01-11 15:50:27 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,1 +1,52 @@
+import { useSelector, useDispatch } from 'react-redux';
+import React,{ useEffect } from 'react';
+import './App.css';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import { login, logout, selectUser } from './userSlice';
+import Login from "./Login";
+import { auth } from "./firebase";
+import Widgets from "./Widgets";
 
+function App() {
+  // TO PULL THE USER FROM THE DATA STORE
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {   //listens to any change in authentication(i.e persisted authentication)
+        if(userAuth){
+            // user is logged in
+            dispatch(  // dispatches user details to store
+            login({
+                email: userAuth.email,
+                uid: userAuth.uid,
+                displayName: userAuth.displayName,
+                photoUrl: userAuth.photoURL,
+            })
+          );
+        } else{
+            // user is logged out
+            dispatch(logout());
+        }
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      {!user ? (
+        <Login />
+        )  :(
+          <div className="app_body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+          </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./Sidebar.css";
+import {Avatar} from "@material-ui/core";
+import { selectUser } from './userSlice';
+import { useSelector } from 'react-redux';
+
+
+function Sidebar () {
+
+    const user = useSelector(selectUser);
+
+    const recentItem = (topic) => (
+        <div className="sidebar_recentItem">
+            <span className="sidebar_hash">#</span>
+            <p>{topic}</p>
+        </div>
+    );
+
+    return (
+        <div className = "sidebar">
+            <div className="sidebar_top">
+                <img src="https://wallpapercave.com/wp/wp2646303.jpg" alt=""/>
+                <Avatar src={ user.photoUrl } className="sidebar_avatar" >{user.email[0]}</Avatar>
+                <h2>{user.displayName}</h2>
+                <h4>{user.email}</h4>
+            </div>
+            <div className="sidebar_stats">
+                <div className="sidebar_stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar_statNumber">2,543</p>
+                </div>
+                <div className="sidebar_stat">
+                    <p>Views on post</p>
+                    <p className="sidebar_statNumber">2,543</p>
+                </div>
+            </div>
+            <div className="sidebar_bottom">
+                <p>Recent</p>
+                {recentItem("reactjs")}
+                {recentItem('programming')}
+                {recentItem('softwareengineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar
